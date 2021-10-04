4549e2e67e77229ea6a6c8a1398676353725a0b6
Peter Stegger <stegger.peter@gmail.com>
2021-10-04 21:23:55 +0200

    Update InMemoryInitializer.cs

--- CoMeta/Data/InMemoryInitializer.cs
+++ CoMeta/Data/InMemoryInitializer.cs
@@ -18,16 +18,17 @@ namespace CoMeta.Data
             // already exists, no action is taken (and no effort is made to ensure it
             // is compatible with the model for this context).
             context.Database.EnsureCreated();
-            
+
             context.Roles.Add(new Role() { Name = "User" });
             context.Roles.Add(new Role() { Name = "Administrator" });
             context.SaveChanges();
             var admin = context.Roles.FirstOrDefault(role => role.Name.Equals("Administrator"));
-            
+
             var authenticationHelper = new AuthenticationHelper(Array.Empty<byte>());
             var password = "password123"; //Why not?
+
+            //Since I'm seeding data I create a password hash + salt manually:
             authenticationHelper.CreatePasswordHash(password, out var pass, out var salt);
-            var uRoles = new List<Role> { admin };
             context.Users.Add(new User()
             {
                 Role = "Administrator",
